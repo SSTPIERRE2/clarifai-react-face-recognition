bd2825a72ff30069fa996b23c95ceecceadf131a
sstpierre2 <stephencstpierre@gmail.com>
2018-07-10 13:51:59 -0400

    Connect frontend to backend server

--- src/components/Rank/Rank.jsx
+++ src/components/Rank/Rank.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 // eslint-disable-next-line
 import tachyons from 'tachyons';
 
-const Rank = () => (
+const Rank = ({ name, entries }) => (
   <div>
     <div className='white f3'>
-      {'Your current rank is...'}
+      {`${name} Your current entry count is...`}
     </div>
     <div className='white f1'>
-      {'#5'}
+      {entries}
     </div>
   </div>
 );
