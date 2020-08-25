d97649a88767f9cc32f6af22e62b5bc9c74925a2
Steve St.Pierre <stephencstpierre@gmail.com>
2020-08-25 11:17:16 -0400

    added rank emoji

--- src/components/Rank/Rank.jsx
+++ src/components/Rank/Rank.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // eslint-disable-next-line
 import tachyons from 'tachyons';
 
-const Rank = ({ name, entries }) => (
-  <div>
-    <div className='white f3'>
-      {`${name} Your current entry count is...`}
+const Rank = ({ name, entries }) => {
+  const [emoji, setEmoji] = useState('');
+
+  useEffect(() => {
+    generateEmoji(entries);
+  }, []);
+
+  const generateEmoji = (entries) => {
+    fetch(`https://9cfiqvzcfl.execute-api.us-east-1.amazonaws.com/dev/rank?rank=${entries}`)
+      .then(resp => resp.json())
+      .then(data => {
+        console.log(`got data`, data);
+        setEmoji(data.input)
+      })
+      .catch(console.log)
+  }
+
+  return (
+    <div>
+      <div className='white f3'>
+        {`${name} Your current entry count is...`}
+      </div>
+      <div className='white f1'>
+        {entries}
+      </div>
+      <div className='white f3'>
+        {`Rank Badge: ${emoji}`}
+      </div>
     </div>
-    <div className='white f1'>
-      {entries}
-    </div>
-  </div>
-);
+  );
+}
 
 export default Rank;
