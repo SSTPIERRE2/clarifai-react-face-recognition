f87571bf438be9038a913ceead5ad70b9c269d75
Steve St.Pierre <stephencstpierre@gmail.com>
2023-05-15 19:15:10 -0400

    update commands

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 export const API_URL =
-  process.env.REACT_APP_NODE_ENV === 'production'
+  process.env.NODE_ENV === 'production'
     ? 'https://smart-brains-clarifai-api.herokuapp.com/'
     : 'http://localhost:3000';
 
