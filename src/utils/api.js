6ec3d2d8b42c315eee495a275faaa05ea152a665
Steve St.Pierre <stephencstpierre@gmail.com>
2023-05-15 15:17:18 -0400

    updated api url, using new heroku url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,26 +1,27 @@
-export const APP_URL = 'http://localhost:3000';
+export const API_URL = 'https://smart-brains-clarifai-api.herokuapp.com/';
+export const LOCAL_API_URL = 'http://localhost:3000';
 
 const makeRequestHeaders = (token) => {
-    const headers = {
-        'Content-Type': 'application/json',
-    };
+  const headers = {
+    'Content-Type': 'application/json',
+  };
 
-    if (token) {
-        headers['Authorization'] = token;
-    }
+  if (token) {
+    headers['Authorization'] = token;
+  }
 
-    return headers;
+  return headers;
 };
 
 export const apiRequest = (route, method, token, body) => {
-    const init = {
-        method,
-        headers: makeRequestHeaders(token)
-    }
+  const init = {
+    method,
+    headers: makeRequestHeaders(token),
+  };
 
-    if (body) {
-        init.body = JSON.stringify(body);
-    }
+  if (body) {
+    init.body = JSON.stringify(body);
+  }
 
-    return fetch(`${APP_URL}/${route}`, init)
-}
+  return fetch(`${API_URL}/${route}`, init);
+};
