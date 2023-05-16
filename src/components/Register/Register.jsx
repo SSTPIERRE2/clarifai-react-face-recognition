ddd96b643ee4e7174cc72e15d434e241aeaeac85
Steve St.Pierre <stephencstpierre@gmail.com>
2023-05-16 10:31:44 -0400

    fix console warnings

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import tachyons from 'tachyons';
 import { saveAuthTokenInSession } from '../../utils/session';
 import { getLoadAuthenticatedUser } from '../../utils/user';
-import '../SignIn/Signin.css'
+import '../SignIn/Signin.css';
 import { apiRequest } from '../../utils/api';
 
 class Register extends Component {
@@ -12,90 +12,100 @@ class Register extends Component {
     this.state = {
       email: '',
       password: '',
-      name: ''
-    }
+      name: '',
+    };
   }
 
   onNameChange = (event) => {
     this.setState({ name: event.target.value });
-  }
+  };
 
   onEmailChange = (event) => {
     this.setState({ email: event.target.value });
-  }
+  };
 
   onPasswordChange = (event) => {
     this.setState({ password: event.target.value });
-  }
+  };
 
-  onSubmitSignIn = () => {
+  onSubmitSignIn = (e) => {
+    e.preventDefault();
     const { name, email, password } = this.state;
 
     apiRequest('register', 'post', null, {
       name,
       email,
-      password
+      password,
     })
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         if (data.userId && data.success === 'true') {
-          saveAuthTokenInSession(data.token)
-          getLoadAuthenticatedUser(data.userId, data.token, this.props.loadUser, this.props.onRouteChange)
+          saveAuthTokenInSession(data.token);
+          getLoadAuthenticatedUser(
+            data.userId,
+            data.token,
+            this.props.loadUser,
+            this.props.onRouteChange
+          );
         }
       })
       .catch((error) => {
         console.log(error);
         this.setState({ loginError: 'Yikes! Network error.' });
       });
-  }
+  };
 
   render() {
     return (
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 center shadow-5">
         <main className="pa4 black-80">
-          <div className="measure">
+          <form className="measure" onSubmit={this.onSubmitSignIn}>
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f1 fw6 ph0 mh0">Register</legend>
               <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
-                <input 
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black" 
-                  type="text" 
-                  name="name"  
+                <label className="db fw6 lh-copy f6" htmlFor="name">
+                  Name
+                </label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black"
+                  type="text"
+                  name="name"
                   id="name"
                   onChange={this.onNameChange}
                 />
               </div>
               <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                <input 
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black" 
-                  type="email" 
-                  name="email-address"  
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                  Email
+                </label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black"
+                  type="email"
+                  name="email-address"
                   id="email-address"
                   onChange={this.onEmailChange}
                 />
               </div>
               <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                <input 
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black" 
-                  type="password" 
-                  name="password"  
+                <label className="db fw6 lh-copy f6" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black"
+                  type="password"
+                  name="password"
                   id="password"
                   onChange={this.onPasswordChange}
+                  autoComplete="on"
                 />
               </div>
             </fieldset>
-            <div className="">
-              <input 
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                type="submit" 
-                value="Register"
-                onClick={this.onSubmitSignIn}
-              />
-            </div>
-          </div>
+            <input
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+              type="submit"
+              value="Register"
+            />
+          </form>
         </main>
       </article>
     );
--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './Signin.css'
+import './Signin.css';
 import { getLoadAuthenticatedUser } from '../../utils/user';
 import { saveAuthTokenInSession } from '../../utils/session';
 import { apiRequest } from '../../utils/api';
@@ -10,16 +10,16 @@ class SignIn extends Component {
     this.state = {
       signInEmail: '',
       signInPassword: '',
-    }
+    };
   }
 
   onEmailChange = (event) => {
     this.setState({ signInEmail: event.target.value });
-  }
+  };
 
   onPasswordChange = (event) => {
     this.setState({ signInPassword: event.target.value });
-  }
+  };
 
   onSubmitSignIn = (e) => {
     e.preventDefault();
@@ -27,15 +27,20 @@ class SignIn extends Component {
 
     apiRequest(`signin`, 'post', null, {
       email: signInEmail,
-      password: signInPassword
+      password: signInPassword,
     })
-      .then(response => {
+      .then((response) => {
         return response.json();
       })
-      .then(data => {
+      .then((data) => {
         if (data.userId && data.success === 'true') {
           saveAuthTokenInSession(data.token);
-          getLoadAuthenticatedUser(data.userId, data.token, this.props.loadUser, this.props.onRouteChange)
+          getLoadAuthenticatedUser(
+            data.userId,
+            data.token,
+            this.props.loadUser,
+            this.props.onRouteChange
+          );
         }
       })
       .catch((error) => {
@@ -44,7 +49,7 @@ class SignIn extends Component {
       });
 
     return false;
-  }
+  };
 
   render() {
     const { onRouteChange } = this.props;
@@ -56,38 +61,42 @@ class SignIn extends Component {
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f1 fw6 ph0 mh0">Sign In</legend>
               <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                <input 
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black" 
-                  type="email" 
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                  Email
+                </label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black"
+                  type="email"
                   name="email-address"
                   id="email-address"
                   onChange={this.onEmailChange}
                 />
               </div>
               <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                <input 
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black" 
-                  type="password" 
-                  name="password"  
+                <label className="db fw6 lh-copy f6" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black"
+                  type="password"
+                  name="password"
                   id="password"
                   onChange={this.onPasswordChange}
+                  autoComplete="on"
                 />
               </div>
             </fieldset>
             <div className="">
-              <input 
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                type="submit" 
+              <input
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                type="submit"
                 value="Sign in"
-                // onClick={() => this.onSubmitSignIn()}
               />
             </div>
             <div className="lh-copy mt3">
-              <p 
-                onClick={() => onRouteChange('register')} 
-                href="#0" 
+              <p
+                onClick={() => onRouteChange('register')}
+                href="#0"
                 className="f6 link dim black db pointer"
               >
                 Register
